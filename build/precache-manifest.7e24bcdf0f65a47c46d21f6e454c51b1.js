95f5f5c1d8cdf7d07a998e87551defdcc3498fe5
Silmilly <powerslab3@gmail.com>
2025-02-11 16:53:16 -0500

    updated deploy command

---
+++ build/precache-manifest.7e24bcdf0f65a47c46d21f6e454c51b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7d87450bdcb34e77f7af208ba6b9eb",
+    "url": "/ach_yip/index.html"
+  },
+  {
+    "revision": "8543fa6b84ed1fda6813",
+    "url": "/ach_yip/static/css/main.f2efdcbe.chunk.css"
+  },
+  {
+    "revision": "18694ac9c2f78f9ed829",
+    "url": "/ach_yip/static/js/2.3c28dfb7.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/ach_yip/static/js/2.3c28dfb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8543fa6b84ed1fda6813",
+    "url": "/ach_yip/static/js/main.6a4bbaf1.chunk.js"
+  },
+  {
+    "revision": "f275ba61b47091a87a5b",
+    "url": "/ach_yip/static/js/runtime-main.e8da25fe.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ach_yip/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ach_yip/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ach_yip/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ach_yip/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ach_yip/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ach_yip/static/media/rating_keydown_5.34958048.png"
+  }
+]);
